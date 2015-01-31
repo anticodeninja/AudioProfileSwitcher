1f28ed8cd83965fab00ae262af3e1d784d0c54b6
Artyom `zZLOiz` Yamshanov <www.zloi@gmail.com>
2015-01-31 22:39:25 +0600

    Initial upload

---
+++ AudioWrapper.cs
@@ -0,0 +1,125 @@
+﻿namespace AudioProfileSwitcher
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class AudioWrapper
+    {
+        #region # Types #
+
+        public enum ERole
+        {
+            Console = 0,
+            Multimedia = 1,
+            Communications = 2,
+            Count = 3
+        }
+
+        public enum EDataFlow
+        {
+            Render = 0,
+            Capture = 1,
+            All = 2,
+            Count = 3
+        }
+
+        [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IMMDevice
+        {
+            int NotImpl1(); int NotImpl2();
+
+            [PreserveSig]
+            int GetId([MarshalAs(UnmanagedType.LPWStr)] out string ppstrId);
+        }
+
+        [Guid("f8679f50-850a-41cf-9c72-430f290290c8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IPolicyConfig
+        {
+            int NotImpl1(); int NotImpl2(); int NotImpl3(); int NotImpl4(); int NotImpl5();
+            int NotImpl6(); int NotImpl7(); int NotImpl8(); int NotImpl9(); int NotImpl10();
+
+            [PreserveSig]
+            int SetDefaultEndpoint(string pszDeviceName, ERole role);
+        }
+
+        [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IMMDeviceEnumerator
+        {
+            int NotImpl1();
+
+            [PreserveSig]
+            int GetDefaultAudioEndpoint(EDataFlow dataFlow, ERole role, out IMMDevice ppEndpoint);
+        }
+
+        [ComImport, Guid("870AF99C-171D-4F9E-AF0D-E63DF40C2BC9")]
+        internal class PolicyConfigClient
+        {
+        }
+
+        [ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+        internal class MMDeviceEnumerator 
+        {
+        }
+
+        #endregion # Types #
+
+        #region # Fields #
+
+        private readonly IMMDeviceEnumerator _realEnumerator;
+        private readonly IPolicyConfig _policyConfig;
+
+        #endregion # Fields #
+
+        #region # Properties #
+
+        public int LoadTimeout { get; set; }
+
+        #endregion # Properties #
+
+        #region # Constructor #
+
+        public AudioWrapper()
+        {
+            LoadTimeout = 10;
+            _realEnumerator = new MMDeviceEnumerator() as IMMDeviceEnumerator;
+            _policyConfig = new PolicyConfigClient() as IPolicyConfig;
+        }
+
+        #endregion # Constructor #
+
+        #region # Methods #
+
+        public string GetDefaultDevice(EDataFlow dataFlow, ERole role)
+        {
+            IMMDevice device = null;
+            try
+            {
+                _realEnumerator.GetDefaultAudioEndpoint(dataFlow, role, out device);
+                string devId;
+                device.GetId(out devId);
+                return devId;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                if (device != null) Marshal.ReleaseComObject(device);
+            }
+        }
+
+        public void SetDefaultDevice(EDataFlow dataFlow, ERole role, string devId)
+        {
+            try
+            {
+                _policyConfig.SetDefaultEndpoint(devId, role);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        #endregion # Methods #
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿namespace AudioProfileSwitcher
+{
+    using System;
+    using System.Threading;
+    using System.Xml;
+    using NDesk.Options;
+
+    internal class Program
+    {
+        private static readonly AudioWrapper.EDataFlow[] DataFlowTypesToWork =
+        {
+            AudioWrapper.EDataFlow.Render,
+            AudioWrapper.EDataFlow.Capture,
+        };
+
+        private static readonly AudioWrapper.ERole[] RoleTypesToWork =
+        {
+            AudioWrapper.ERole.Console,
+            AudioWrapper.ERole.Multimedia,
+            AudioWrapper.ERole.Communications,
+        };
+
+        private static void SetWithTimeout(AudioWrapper wrapper, AudioWrapper.EDataFlow flow, AudioWrapper.ERole role,
+            string devId, int timeout)
+        {
+            var endTime = Environment.TickCount + timeout*1000;
+            for (;;)
+            {
+                wrapper.SetDefaultDevice(flow, role, devId);
+                if (wrapper.GetDefaultDevice(flow, role) == devId || (timeout != -1 && Environment.TickCount > endTime))
+                    return;
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void LoadSettings(string path, int timeout)
+        {
+            var wrapper = new AudioWrapper();
+
+            var xml = new XmlDocument();
+            xml.Load(path);
+
+            foreach (var flow in DataFlowTypesToWork)
+            {
+                foreach (var role in RoleTypesToWork)
+                {
+                    var selectSingleNode = xml.SelectSingleNode(string.Format("/config/{0}-{1}", flow, role));
+                    if (selectSingleNode == null) continue;
+                    SetWithTimeout(wrapper, flow, role, selectSingleNode.InnerText, timeout);
+                }
+            }
+        }
+
+        public static void SaveSettings(string path, int timeout)
+        {
+            var wrapper = new AudioWrapper();
+
+            var xml = new XmlDocument();
+            xml.LoadXml("<config></config>");
+            
+            foreach (var flow in DataFlowTypesToWork)
+            {
+                foreach (var role in RoleTypesToWork)
+                {
+                    var node = xml.CreateElement(string.Format("{0}-{1}", flow, role));
+                    node.InnerText = wrapper.GetDefaultDevice(flow, role);
+                    xml.DocumentElement.AppendChild(node);
+                }
+            }
+
+            xml.Save(path);
+        }
+
+        static void Main(string[] args)
+        {
+            var timeout = 10;
+            var filename = "default.xml";
+            Action<string, int> action = SaveSettings;
+
+            var p = new OptionSet
+            {
+                {"t|timeout=", (int value) => timeout = value},
+                {"l|load=", path => { filename = path; action = LoadSettings; }},
+                {"s|save=", path => { filename = path; action = SaveSettings; }},
+            };
+            p.Add("?|help", v => p.WriteOptionDescriptions(Console.Out));
+            p.Parse(args);
+
+            action(filename, timeout);
+        }
+    }
+}
